4cb34c4ff8176c38f900908470fa478daa9b5131
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-11 21:39:32 +0500

    Added basic implementation of customer class

---
+++ customer.py
@@ -0,0 +1,22 @@
+class Customer():
+    """Contains the information about the customer and it's accounts"""
+
+    def __init__(self, customer_id, name, phone):
+        """Initilize the customer attributes"""
+        self.customer_id = customer_id
+        self.name = name
+        self.phone = phone
+        self.accounts = []
+
+    def add_account(self, account):
+        """Adds the account in the customer account list."""
+        self.accounts.append(account)
+
+    def remove_account(self, account):
+        """Removes the account from the customer accounts list."""
+        self.accounts.remove(account)
+
+    def show_accounts(self):
+        """Shows the accounts created under customer's name"""
+        for account in self.accounts:
+            print(account)
