e700026ea7e56c643702de61d61b28a2c014815b
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-12 18:55:46 +0500

    Added attributes and methods to the bank class

--- bank.py
+++ bank.py
@@ -6,3 +6,30 @@ class Bank():
         self.name = name
         self.customers = []
         self.accounts = []
+
+    def add_customer(self, customer):
+        """Add customer into the customer list."""
+        pass
+
+    def remove_customer(self, customer):
+        """Remove the given customer from the list."""
+        pass
+
+    def find_customer(self, customer_id):
+        """Find the customer using the id"""
+        pass
+
+    def add_account(self, account):
+        """Add account to the accounts list."""
+        pass
+
+    def remove_account(self, account):
+        """Remove the given customer from the list."""
+
+    def find_account(self, account_number):
+        """Find the account using the account number."""
+        pass
+
+    def transfer_money(self, sender_account, receiver_account, amount):
+        """Transfer the given ammount to the recieving account."""
+        pass
