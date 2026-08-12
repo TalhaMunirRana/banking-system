f37c21fe5afbf7c060ab982e81243651fd717779
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-12 18:49:07 +0500

    Added the bank class basics

---
+++ bank.py
@@ -0,0 +1,8 @@
+class Bank():
+    """A Simple attempt to model a Bank"""
+
+    def __init__(self, name):
+        """Initialize the attirbutes for bank."""
+        self.name = name
+        self.customers = []
+        self.accounts = []
