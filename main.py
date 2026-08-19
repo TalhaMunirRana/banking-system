74fbc070a909a6f6b57a4deae885b408fdfc3f61
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-19 20:51:26 +0500

    Refactored the code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+from bank import Bank
+from account import Account
+from customer import Customer
 
 
-def main():
-    bank = Bank("Python National Bank")
+bank = Bank("Python National Bank")
 
-    while True:
-        print("\n" + "=" * 37)
-        print(f"{bank} - Banking System")
-        print("=" * 37)
+while True:
+    print("\n" + "=" * 37)
+    print(f"{bank} - Banking System")
+    print("=" * 37)
 
-
-main()
