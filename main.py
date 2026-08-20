57c8867db37282e56d1301de19c711eaa7bf39de
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-20 21:09:02 +0500

    Added base logic

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ while True:
     print(f"{bank} - Banking System")
     print("=" * 37)
 
+    print("""
+1. Add customer
+2. Create Account
+3. Deposit
+4. Withdraw
+5. Check Balance
+6. Transfer Money
+7. Show Transactions
+8. Show Customer Accounts
+9. Find Customer
+10. Find Account
+11. Close Account
+12. Exit
+""")
+
